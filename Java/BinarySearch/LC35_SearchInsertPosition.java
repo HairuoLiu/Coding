c304d0bb05018a9e66b5ca95e4302e704c8f0c49
HairuoLiu <liu.3502@osu.edu>
2019-03-24 12:05:26 -0700

    3-24-12:15AM

--- Java/BinarySearch/LC35_SearchInsertPosition.java
+++ Java/BinarySearch/LC35_SearchInsertPosition.java
@@ -1,40 +1,21 @@
 
 /**
- * Given a list of sorted characters letters containing only lowercase letters,
- * and given a target letter target, find the smallest element in the list that is larger than the given target.
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+ * You may assume no duplicates in the array.
+ * Example 1:
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ * Example 2:
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ * Example 3:
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ * Example 4:
  *
- * Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
- *
- * Examples:
- * Input:
- * letters = ["c", "f", "j"]
- * target = "a"
- * Output: "c"
- *
- * Input:
- * letters = ["c", "f", "j"]
- * target = "c"
- * Output: "f"
- *
- * Input:
- * letters = ["c", "f", "j"]
- * target = "d"
- * Output: "f"
- *
- * Input:
- * letters = ["c", "f", "j"]
- * target = "g"
- * Output: "j"
- *
- * Input:
- * letters = ["c", "f", "j"]
- * target = "j"
- * Output: "c"
- *
- * Input:
- * letters = ["c", "f", "j"]
- * target = "k"
- * Output: "c"
+ * Input: [1,3,5,6], 0
+ * Output: 0
  *
  * @authorLiu.3502
  * @created2018-01-31 下午6:45
@@ -48,7 +29,26 @@ public class LC35_SearchInsertPosition{
     char ans = nextGreatestLetter(letter,target);
     System.out.println(ans);
   }
+  
+  //
+  public int searchInsert(int[] nums, int target) {
+    //sanity check
+    if(nums == null || nums.length == 0) return 0;
+    int left = 0, right = nums.length - 1 ;
+    int res = -1;
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+        if(nums[mid] >= target){
+            right = mid - 1;
+            res = mid;
+        }else{
+            left = mid + 1;
+        }
+    }
+    return res == -1 ? nums.length : res;
+  }
 
+  //变形题 
   public static char nextGreatestLetter(char[] letters, char target) {
       int left = 0, right = letters.length - 1;
       //sanity cheak
